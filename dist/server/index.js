159d84047703b55a146ede8e790642813ecb2b99
ikovac <ikovacevic96k@gmail.com>
2021-03-12 11:04:07 +0100

    Move assets path as the storage method ♻️

--- dist/server/index.js
+++ dist/server/index.js
@@ -2,7 +2,7 @@ const find = require('lodash/find');
 const xmlParser = require('fast-xml-parser');
 
 async function setLaunchUrl(element, { config, storage, storageProxy }) {
-  const { protocol, path } = config.storage;
+  const { protocol } = config.storage;
   const { root, assets } = element.data;
   if (!root) return element;
   const manifestKey = find(assets, it => it.endsWith('imsmanifest.xml'));
@@ -10,7 +10,8 @@ async function setLaunchUrl(element, { config, storage, storageProxy }) {
   const options = { ignoreAttributes: false, attributeNamePrefix: '$_' };
   const { manifest: parsedManifest } = xmlParser.parse(manifest.toString(), options);
   const { $_href: resourcePath } = parsedManifest.resources.resource;
-  const launchKey = `repository/${element.repositoryId}/${path}/${root}/${resourcePath}`;
+  const prefix = storage.getAssetsPath(element.repositoryId);
+  const launchKey = `${prefix}/${root}/${resourcePath}`;
   element.data.launchUrl = storageProxy.getFileUrl(launchKey);
   return element;
 }
